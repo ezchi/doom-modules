4b84c0a4c7fea684ea72820d71e07b8124ded0de
Enze Chi <enze.chi@gmail.com>
2019-08-20 21:52:55 +1000

    Wrap config into 'after!'

--- modules/private/vhdlplus/config.el
+++ modules/private/vhdlplus/config.el
@@ -1,12 +1,13 @@
 ;;; vhdlplus/config.el -*- lexical-binding: t; -*-
 
-(require 'lsp)
-(lsp-register-client
- (make-lsp-client :new-connection (lsp-stdio-connection
-                                   (lambda () (cons "vhdl-tool" '("lsp"))))
-                  :major-modes '(vhdl-mode)
-                  :priority 0
-                  :server-id 'vhdl-ls))
+(after! vhdl-mode
+  (after! lsp
+    (lsp-register-client
+     (make-lsp-client :new-connection (lsp-stdio-connection
+                                       (lambda () (cons "vhdl-tool" '("lsp"))))
+                      :major-modes '(vhdl-mode)
+                      :priority 0
+                      :server-id 'vhdl-ls))
 
-(add-hook 'vhdl-mode-hook #'lsp)
-(add-to-list 'lsp-language-id-configuration '(vhdl-mode . "vhdl"))
+    (add-to-list 'lsp-language-id-configuration '(vhdl-mode . "vhdl")))
+  (add-hook 'vhdl-mode-hook #'lsp))
