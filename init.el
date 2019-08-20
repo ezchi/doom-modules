374154925a283b8915c16f15a07ec747d1489729
Enze Chi <enze.chi@gmail.com>
2019-08-20 21:23:33 +1000

    Enable more features from ivy module

--- init.el
+++ init.el
@@ -20,7 +20,12 @@
        company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ivy               ; a search engine for love and life
+       (ivy
+        +childframe
+        +fuzzy
+        +icons
+        +prescient
+        )          ; a search engine for love and life
 
        :ui
        ;;deft              ; notational velocity for Emacs
