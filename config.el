a5c2236635e60123dc218830c047e343dfc17837
Enze Chi <ezchi@Enzes-MacBook-Pro.local>
2020-05-04 21:03:16 +1000

    Disable private org module
    Disable private org module and put temporary configuration in config.el.
    The org related configuraiton should be moved to private org module
    someday.

--- config.el
+++ config.el
@@ -1,3 +1,8 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
+
+;; Org
+(after! org
+  (setq org-log-into-drawer t)
+  (setq org-log-state-notes-into-drawer t))
