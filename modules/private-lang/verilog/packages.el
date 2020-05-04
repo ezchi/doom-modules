a6d82e1bc52df0187873e41f197e0cb96528d815
Enze Chi <ezchi@Enzes-MacBook-Pro.local>
2020-05-04 20:51:19 +1000

    Use verilog-mode from "lite" branch

--- modules/private-lang/verilog/packages.el
+++ modules/private-lang/verilog/packages.el
@@ -1,5 +1,10 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; private-lang/verilog/packages.el
 
-(package! verilog-mode :recipe (:host github :repo "ezchi/verilog-mode" :branch "develop"))
-(package! yasnippets-verilog :recipe (:host github :repo "ezchi/yasnippets-verilog" :files ("*.el" ("snippets" ("verilog-mode" "snippets/verilog-mode/*")))))
+(package! verilog-mode
+  :recipe (:host github :repo "ezchi/verilog-mode" :branch "lite"
+           :files ("verilog-mode.el")))
+
+(package! yasnippets-verilog
+  :recipe (:host github :repo "ezchi/yasnippets-verilog"
+           :files ("*.el" ("snippets" ("verilog-mode" "snippets/verilog-mode/*")))))
