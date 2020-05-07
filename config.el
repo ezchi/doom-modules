c2eb46e49a822bcfea4d5c86a65d12aa43d2fbe4
Enze Chi <ezchi@Enzes-MacBook-Pro.local>
2020-05-08 08:25:26 +1000

    Set font to Go Mono for Powerline

--- config.el
+++ config.el
@@ -1,6 +1,10 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
+;; Fonts
+(if IS-MAC
+    (setq doom-font (font-spec :family "Go Mono for Powerline" :size 12))
+  (setq doom-font (font-spec :family "Go Mono for Powerline" :size 22)))
 
 ;; Org
 (after! org
