c3d27281f3e342478ed5c4ebc5821e1d30e0d364
Enze Chi <ezchi@Enzes-MacBook-Pro.local>
2020-05-11 21:38:59 +1000

    Remove +childframe and +icon flags
    There's significant performance issue on Linux. Disable +childframe and
    +icon flags to try if they are the root cause.

--- init.el
+++ init.el
@@ -19,10 +19,10 @@
        ;;japanese
 
        :completion
-       (company +childframe)           ; the ultimate code completion backend
+       (company)           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy +childframe +icons)               ; a search engine for love and life
+       (ivy)               ; a search engine for love and life
 
        :ui
        ;;deft              ; notational velocity for Emacs
@@ -63,7 +63,7 @@
        ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
-       (dired +icons)             ; making dired pretty [functional]
+       (dired)             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        ;;ibuffer         ; interactive buffer management
        (undo +tree)              ; persistent, smarter undo for your inevitable mistakes
