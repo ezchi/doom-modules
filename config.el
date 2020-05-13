2a95ed25aeadd25f6118280b05ef786d4df37006
Enze Chi <ezchi@Enzes-MacBook-Pro.local>
2020-05-13 20:27:45 +1000

    Display time with date

--- config.el
+++ config.el
@@ -6,6 +6,9 @@
     (setq doom-font (font-spec :family "Go Mono for Powerline" :size 12))
   (setq doom-font (font-spec :family "Go Mono for Powerline" :size 22)))
 
+(setq display-time-day-and-date t)
+(display-time-mode t)
+
 ;; Org
 (after! org
   (setq org-log-into-drawer t)
